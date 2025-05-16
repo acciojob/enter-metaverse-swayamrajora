2c8098e32f3b0f89a25016554646c2572a91b4de
swayamrajora <127737419+swayamrajora@users.noreply.github.com>
2025-05-16 07:49:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.getElementById("enterBtn").addEventListener("click", function() {
     let statusElement = document.getElementById("status");
-    statusElement.innerHTML = "<h1>Entered Metaverse</h1>"; 
+    statusElement.innerHTML = "<h1>Entered Metaverse</h1>"; // Change text to "Entered Metaverse" within an <h1> tag
+});
